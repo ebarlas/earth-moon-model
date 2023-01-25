49880a5a5954c2077e2c670ed2836776c24bb470
Elliot Barlas <elliotbarlas@gmail.com>
2023-01-24 19:20:00 -0800

    Configure sleep per motor.

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ logger = logging.getLogger(__name__)
 
 STEPS_PER_REV = 200
 
-SLEEP = 0
+EO_SLEEP = 0.1
+ER_SLEEP = 0.05
+MO_SLEEP = 0.05
 
 
 def turn_off_motors(steppers):
@@ -39,16 +41,16 @@ def main():
     kit = MotorKit()
     kit2 = MotorKit(address=0x61)
 
-    eo_motor = motor.Motor(kit.stepper1, sensor.Sensor(17), SLEEP, STEPS_PER_REV)
-    er_motor = motor.Motor(kit.stepper2, sensor.Sensor(27), SLEEP)
-    mo_motor = motor.Motor(kit2.stepper1, sensor.Sensor(23), SLEEP)
+    eo_motor = motor.Motor(kit.stepper1, sensor.Sensor(17), EO_SLEEP, STEPS_PER_REV)
+    er_motor = motor.Motor(kit.stepper2, sensor.Sensor(27), ER_SLEEP)
+    mo_motor = motor.Motor(kit2.stepper1, sensor.Sensor(23), MO_SLEEP)
 
     atexit.register(turn_off_motors, [kit.stepper1, kit.stepper2, kit2.stepper1])
 
     eo_model = model.Model(eo_motor, er_motor, mo_motor, logger, STEPS_PER_REV)
     eo_model.init()
     while True:
-        eo_model.next(earth.timescale.now())
+        eo_model.next(earth.earth(earth.timescale.now()))
         time.sleep(60)
 
 
